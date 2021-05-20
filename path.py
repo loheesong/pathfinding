0f25c382077d75fa6ff600382f1cea2021989560
Ee Song <loheesong@gmail.com>
2021-05-20 14:59:15 +0800

    200521

--- path.py
+++ path.py
@@ -1,6 +1,7 @@
 import pygame, tkinter
 
 WIDTH, HEIGHT = 650, 650
+ROW, COL = 50, 50 
 FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pathfinding")
@@ -16,41 +17,66 @@ class Grid:
         self.col = col
         self.grid = [[Box(row, col) for col in range(self.col)] for row in range(self.row)]
 
-    def render(self, win):
-        win.fill(WHITE)
 
+    def render(self, win):
         gap = WIDTH / self.col
+
+        for row in self.grid:
+            for col in row:
+                col.render(win)
+
         for i in range(self.col + 1):
             # vertical lines
             pygame.draw.line(win, GREY, (i * gap, 0), (i * gap, HEIGHT))
             # horizontal lines
             pygame.draw.line(win, GREY, (0, i * gap), (WIDTH, i * gap))
+        
+        
     
-    def on_click(self):
-        pass
-
+    def on_click(self, mouse_pos, mouse_button):
+        """Select start and end points. Make walls delete walls"""
+        row = int(mouse_pos[1] // (HEIGHT / self.row))
+        col = int(mouse_pos[0] // (WIDTH / self.col))
+        if mouse_button == "left":
+            self.grid[row][col].colour = BLACK
+        elif mouse_button == "right":
+            self.grid[row][col].colour = WHITE
 
 class Box:
     def __init__(self, row, col):
         self.row = row
         self.col = col
+        self.colour = WHITE
 
-    def render(self):
-        pass
+        self.x = WIDTH / COL * self.col
+        self.y = HEIGHT / ROW * self.row
+        self.width = WIDTH / COL 
+        self.height = HEIGHT / ROW
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def render(self, win):
+        pygame.draw.rect(win, self.colour, self.rect)
 
 def main():
     clock = pygame.time.Clock()
+    grid = Grid(ROW, COL)
+
     run = True 
     gameState = "run"
-    grid = Grid(50,50)
-
+    start = None
+    end = None
+    
     while run:
         clock.tick(FPS)
         
 
         for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONUP:
-                grid.on_click()
+            if pygame.mouse.get_pressed()[0]: # LEFT
+                mouse_button = "left"
+                grid.on_click(pygame.mouse.get_pos(), mouse_button)
+            elif pygame.mouse.get_pressed()[2]: # right
+                mouse_button = "right"
+                grid.on_click(pygame.mouse.get_pos(), mouse_button)
 
             if event.type == pygame.QUIT:
                 run = False
